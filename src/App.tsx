d91792cec649541af94e27212b9961828956470a
Szymon Brud <szymonqqaz22@gmail.com>
2021-10-09 17:08:12 +0200

    Delete default files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,5 @@
 import React, { ReactElement } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Comp from 'Comp';
 
-const App = (): ReactElement => (
-  <div className="App">
-    <Comp />
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+const App = (): ReactElement => <h1>Hello World</h1>;
 
 export default App;
--- src/Comp.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactElement } from 'react';
-
-const Comp = (): ReactElement => <h1>Hello</h1>;
-
-export default Comp;
